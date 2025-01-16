7a80c089aa8e489400b89cb38f37681563e0c9b9
Ernan21 <ernando.freitas123@Gmail.com>
2025-01-16 03:45:58 -0300

    atualização de layout, melhoria de carregamento, ajustes no script primcipal

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     const calcularIdade = (dataNascimento) => {
         const hoje = new Date();
         const nascimento = new Date(dataNascimento);
         let idade = hoje.getFullYear() - nascimento.getFullYear();
         const mes = hoje.getMonth() - nascimento.getMonth();
-
-        // Ajusta a idade se o aniversário ainda não aconteceu este ano
         if (mes < 0 || (mes === 0 && hoje.getDate() < nascimento.getDate())) {
             idade--;
         }
-
         return idade;
     };
 
     const exibirIdade = () => {
-        const dataNascimento = "2001-06-26"; // Substitua pela data desejada no formato YYYY-MM-DD
-        const idade = calcularIdade(dataNascimento);
-        document.getElementById("idade").textContent = `${idade}`;
+        const dataNascimento = "2001-06-26";
+        document.getElementById("idade").textContent = calcularIdade(dataNascimento);
     };
 
     exibirIdade();
 });
+
+function abrirModal(src, title) {
+    const modal = document.getElementById("modal");
+    const iframe = document.getElementById("iframe");
+    iframe.src = src;
+    iframe.title = title
+    modal.style.display = "block";
+}
+
+function fecharModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+    document.getElementById("iframe").src = "";
+}
